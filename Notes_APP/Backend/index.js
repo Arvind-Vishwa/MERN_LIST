9b4dce5414e551da1cb1c63168fa13f891ccf231
Arvind-Vishwa <av0216380@gmail.com>
2026-01-04 16:57:12 +0530

    add gitignore

--- Notes_APP/Backend/index.js
+++ Notes_APP/Backend/index.js
@@ -1,7 +1,14 @@
 const express=require('express')
 const app=express()
 
+app.get('/add',(req,res) =>{
+    res.send('file added');
+})
+
+app.post('/add',(req,res)=>{
+    res.send('sucess');
+})
 
 app.listen(8080,()=>{
     console.log(`Server is started`);
-})
+});
