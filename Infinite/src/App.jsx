7d9f694c2d2b5a8b249a8ec9479b49272b33fe57
Arvind-Vishwa <av0216380@gmail.com>
2026-01-24 14:43:46 +0530

    first commit

---
+++ Infinite/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  
+  return (
+    <div>
+      <div className='outer'>
+        Making a Onscroll 
+      </div>
+    </div>
+  )
+}
+
+export default App
