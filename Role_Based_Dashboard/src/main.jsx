405f39c415a91943d025af8cee4850dc9fb81da2
Arvind-Vishwa <av0216380@gmail.com>
2026-02-11 16:21:28 +0530

    import context and route

--- Role_Based_Dashboard/src/main.jsx
+++ Role_Based_Dashboard/src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
+
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css'
+import { BrowserRouter } from 'react-router-dom';
+import ContextData from './ContextAPI/ContextData.jsx';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  
+    <BrowserRouter>
+    <ContextData>
+        <App />
+    </ContextData>
+    </BrowserRouter>
+  
 )
