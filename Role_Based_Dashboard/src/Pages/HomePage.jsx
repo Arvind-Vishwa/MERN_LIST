c9a25a4eb70a7e98b18710f9de6b79ed83a9f23f
Arvind-Vishwa <av0216380@gmail.com>
2026-02-16 16:11:51 +0530

    Updated HomePage

---
+++ Role_Based_Dashboard/src/Pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import NavBar from './NavBar'
+import Cards from '../component/Cards'
+
+const HomePage = () => {
+  return (
+    <div>
+      <NavBar />
+      <Cards />
+    </div>
+  )
+}
+
+export default HomePage
